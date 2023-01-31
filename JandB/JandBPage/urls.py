164ca12783a981ed0108b7fb5dc8c02f4736a6b9
kickJS22 <bustillosjuancho+github@gmail.com>
2023-01-30 22:35:04 -0300

    testing 4.0

--- JandB/JandBPage/urls.py
+++ JandB/JandBPage/urls.py
@@ -6,7 +6,7 @@ from . import views
 app_name = "JandB"
 
 urlpatterns = [
-    path("JandB", views.index, name="index"),
+    path("https://kickjs22.github.io/JandB/", views.index, name="index"),
     path("about", views.home, name="home"),
     path("mywork", views.mywork, name="mywork"),
     path("knowledge", views.knowledge, name="knowledge")
